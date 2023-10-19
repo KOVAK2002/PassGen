0c11268c665152d27b58521ed547588816222667
kova <bornakovacevic5@gmail.com>
2023-10-19 09:56:17 +0200

    Dodane stranice Generate i Check

---
+++ Pages/Generate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PassGen.Pages
+{
+    public class GenerateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
